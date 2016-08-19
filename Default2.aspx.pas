eef5fd30fff2c7a22bfc595828855147005c5e70
David Cresswell <dgc@estechco.co.uk>
2016-08-19 15:13:22 +0100

    Background pas files added

---
+++ Default2.aspx.pas
@@ -0,0 +1,26 @@
+namespace ng1AspNet;
+
+interface
+
+uses 
+  System,
+  System.Collections.Generic,
+  System.Linq,
+  System.Web,
+  System.Web.UI,
+  System.Web.UI.WebControls;
+
+type
+  Default2 = public partial class(System.Web.UI.Page)
+  protected
+    method Page_Load(sender : Object; e : EventArgs);
+  end;
+
+implementation
+  
+method Default2.Page_Load(sender : Object; e : EventArgs);
+begin
+  
+end;
+  
+end.
